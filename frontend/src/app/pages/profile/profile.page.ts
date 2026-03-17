82ee68ee212439e6edb4a56f2d263ff6e46cd7b1
VictorHazebrouck <hazebrouck.victor@gmail.com>
2026-03-17 18:53:16 +0100

    ui: fix ui

--- frontend/src/app/pages/profile/profile.page.ts
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -1,9 +1,23 @@
 import { Component, signal } from "@angular/core";
 import { HeaderFullComponent } from "../../components/header-full/header-full.component";
+import { ThemeCardComponent } from "../../components/theme-card/theme-card.component";
+import { TextInputComponent } from "../../components/ui/text-input/text-input.component";
+import { ScrollComponent } from "../../components/ui/scroll/scroll.component";
+import { GridLayout } from "../../components/layouts/grid-layout/grid.layout";
+import { ButtonComponent } from "../../components/ui/button/button.component";
+import { ThreePartsLayout } from "../../components/layouts/three-parts-layout/three-parts.layout";
 
 @Component({
   selector: "profile-page",
-  imports: [HeaderFullComponent],
+  imports: [
+    HeaderFullComponent,
+    ThemeCardComponent,
+    TextInputComponent,
+    ScrollComponent,
+    GridLayout,
+    ButtonComponent,
+    ThreePartsLayout,
+  ],
   templateUrl: "./profile.page.html",
   styleUrls: [],
 })
