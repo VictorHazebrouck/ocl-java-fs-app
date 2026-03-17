bde079016a3f08518a6044b9928e27d9f4364d85
VictorHazebrouck <hazebrouck.victor@gmail.com>
2026-03-17 18:19:59 +0100

    ui: working on aritcle by id

---
+++ frontend/src/app/components/comment-card/comment-card.component.ts
@@ -0,0 +1,14 @@
+import { NgClass } from "@angular/common";
+import { Component, input } from "@angular/core";
+
+@Component({
+  selector: "comment-card-component",
+  imports: [NgClass],
+  templateUrl: "./comment-card.component.html",
+  host: { style: "display: contents;" },
+})
+export class CommentCardComponent {
+  class = input("");
+  username = input("Username");
+  content = input("Contenu du commentaire");
+}
--- frontend/src/app/pages/article-by-id/article-by-id.page.ts
+++ frontend/src/app/pages/article-by-id/article-by-id.page.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from "@angular/core";
+import { Component, input } from "@angular/core";
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { lucideArrowLeft, lucideSend } from "@ng-icons/lucide";
+import { CommentCardComponent } from "../../components/comment-card/comment-card.component";
 import { HeaderFullComponent } from "../../components/header-full/header-full.component";
+import { ScrollComponent } from "../../components/ui/scroll/scroll.component";
+import { TextareaComponent } from "../../components/ui/textarea/textarea.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "article-by-id-page",
-  imports: [HeaderFullComponent],
+  imports: [
+    HeaderFullComponent,
+    ScrollComponent,
+    NgIcon,
+    CommentCardComponent,
+    TextareaComponent,
+    RouterLink,
+  ],
+  viewProviders: [provideIcons({ lucideArrowLeft, lucideSend })],
   templateUrl: "./article-by-id.page.html",
   host: { style: "display: contents;" },
 })
 export class ArticleByIdPage {
-  protected readonly title = signal("mddweb");
+  class = input("");
+  title = input("Title");
+  date = input("Date");
+  author = input("Author");
+  topic = input("Topic");
+  content = input(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  );
 }
